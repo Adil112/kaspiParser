63dbe97d2be431ca3de69ac61fb7ca69b0a75058
adil <adil.200191@gmail.com>
2025-05-12 14:24:25 +0500

    1. parse all shops 2. save shops in one file 3. update work with thread, add wait until and change quantity of threads 4. Trying make parsing with batches, but I cant put direct url to different page 5. add pauses between pages

--- src/main/java/parser.java
+++ src/main/java/parser.java
@@ -6,22 +6,24 @@ import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.NoSuchElementException;
 
 import java.io.BufferedWriter;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
 import java.lang.Thread;
+import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import org.openqa.selenium.TimeoutException;
 
 // 1. Скопировать ссылку на карточку.
 // 2. Открыть новое окно по ссылке.
@@ -32,9 +34,19 @@ import java.util.regex.Pattern;
 // 7. Закрыть старое окно, повторить пункт 1.
 // 8. Если не ссылки на следующую страницу, закрыть окно.
 
-class ProductParseThread extends Thread {
-    public final static ChromeOptions chromeOptions = new ChromeOptions().addArguments("--headless").addArguments("--log-level=3");
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import java.io.*;
+import java.util.*;
 
+class ProductParseThread extends Thread {
+    public final static ChromeOptions chromeOptions = new ChromeOptions()
+            .addArguments("--headless")
+            .addArguments("--log-level=3");
     private CountDownLatch latch;
     private String name;
     private String href;
@@ -47,66 +59,82 @@ class ProductParseThread extends Thread {
 
     @Override
     public void run() {
-        Product product = new Product(this.name);
-
+        Product product = new Product();
         String link = this.href;
         WebDriver cardDriver = new ChromeDriver();
         cardDriver.get(link);
 
-        List<WebElement> shops = cardDriver.findElements(By.xpath("//td/a"));
+        WebDriverWait wait = new WebDriverWait(cardDriver, Duration.ofSeconds(5));
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td/a")));
+        } catch (TimeoutException e) {
+            System.out.println("Не дождались загрузки продавцов");
+        }
 
-        for (int j = 0; j < shops.size(); j++) {
-            String shopName = shops.get(j).getText();
-            String shopHref = shops.get(j).getAttribute("href");
+        Set<String> sellerLinks = new HashSet<>();
 
-            // Переход на страницу продавца
+        while (true){
+            List<WebElement> shops = cardDriver.findElements(By.xpath("//td/a"));
+            for (WebElement shop : shops) {
+                String shopHref = shop.getAttribute("href");
+                if (shopHref != null && !shopHref.isEmpty() && !sellerLinks.contains(shopHref)) {
+                    sellerLinks.add(shopHref);
+                }
+            }
+
+            break; // убрать и вернуть ниже, только первые 5 продвацов берет для тестирования
+
+//            try {
+//                WebElement almatyOption = cardDriver.findElement(By.xpath("//a[@data-city-id='750000000']"));
+//                almatyOption.click();
+//                Thread.sleep(1000);
+//            } catch (Exception ignored) {}
+//
+//
+//            try {
+//                WebElement nextBtn = cardDriver.findElement(By.xpath("//li[contains(text(), 'Следующая')]"));
+//                if (nextBtn.getAttribute("class").contains("disabled")) {
+//                    break;
+//                }
+//                nextBtn.click();
+//                Thread.sleep(1000);
+//            } catch (Exception e) {
+//                break;
+//            }
+        }
+
+        for (String shopHref : sellerLinks) {
             WebDriver shopDriver = new ChromeDriver();
             shopDriver.get(shopHref);
-
-            String sellerName = "";
-            String phoneNumber = "";
-            String dateFrom = "";
-            String rating = "";
-
+            String sellerName = "", phoneNumber = "", dateFrom = "", rating = "";
 
             try {
-                WebElement sellerNameElement = shopDriver.findElement(By.className("merchant-profile__title"));
-                sellerName = sellerNameElement.getText();
-            } catch (NoSuchElementException e) {
-            }
+                sellerName = shopDriver.findElement(By.className("merchant-profile__title")).getText();
+            } catch (NoSuchElementException ignored) {}
 
             try {
-                WebElement phoneElement = shopDriver.findElement(By.className("merchant-profile__contact-text"));
-                phoneNumber = phoneElement.getText();
-            } catch (NoSuchElementException e) {
-            }
+                phoneNumber = shopDriver.findElement(By.className("merchant-profile__contact-text")).getText();
+            } catch (NoSuchElementException ignored) {}
 
             try {
-                WebElement dateFromElement = shopDriver.findElement(By.className("merchant-profile__register-date"));
-                dateFrom = dateFromElement.getText();
-            } catch (NoSuchElementException e) {
-            }
+                dateFrom = shopDriver.findElement(By.className("merchant-profile__register-date")).getText();
+            } catch (NoSuchElementException ignored) {}
 
             try {
                 WebElement ratingElement = shopDriver.findElement(By.cssSelector(".merchant-profile__rating.rating._seller"));
-                String className = ratingElement.getAttribute("class");
-                Pattern pattern = Pattern.compile("_(\\d+)$");
-                Matcher matcher = pattern.matcher(className);
+                Matcher matcher = Pattern.compile("_(\\d+)$").matcher(ratingElement.getAttribute("class"));
                 if (matcher.find()) {
-                    String ratingString = matcher.group(1);
-                    double ratingDouble = Double.parseDouble(ratingString) / 10.0;
+                    double ratingDouble = Double.parseDouble(matcher.group(1)) / 10.0;
                     rating = String.valueOf(ratingDouble);
                 }
-            } catch (NoSuchElementException e) {
-            }
+            } catch (NoSuchElementException ignored) {}
+
             shopDriver.quit();
             Deal deal = new Deal(sellerName.replaceAll(" в городе", ""), phoneNumber, dateFrom.replaceAll("В Kaspi Магазине с ", ""), rating);
             product.addDeal(deal);
-
         }
-
         parser.addProduct(product);
-        shops.clear();
+        sellerLinks.clear();
         cardDriver.quit();
         this.latch.countDown();
     }
@@ -143,11 +171,9 @@ class Deal {
 }
 
 class Product {
-    private String name;
     private ArrayList<Deal> deals = new ArrayList<>();
 
-    public Product(String name) {
-        this.name = name;
+    public Product() {
     }
 
     public void addDeal(Deal deal) {
@@ -158,18 +184,19 @@ class Product {
         return this.deals;
     }
 
-    public String getName() {
-        return this.name;
-    }
 }
 
 public class parser {
 
     public final static String categoryPath = "https://kaspi.kz/shop/c/beauty%20care/";
 
-    public final static ChromeOptions chromeOptions = new ChromeOptions().addArguments("--headless").addArguments("--log-level=3");
+    public final static ChromeOptions chromeOptions = new ChromeOptions()
+            .addArguments("--headless")
+            .addArguments("--log-level=3");
 
     public static boolean nextFlag = false;
+
+    static String exportFilePath = "deals_export.txt";
 
     public static ArrayList<Product> products = new ArrayList<>();
 
@@ -182,38 +209,27 @@ public class parser {
     }
 
     public static void writeProducts() throws IOException {
-        File exportFile = new File(Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
-                + "-" + Calendar.getInstance().get(Calendar.MONTH)
-                + "-" + new Random().nextInt(10001)
-                + ".txt");
-        FileWriter exportWriter = new FileWriter(exportFile);
+        FileWriter exportWriter = new FileWriter(exportFilePath, true); // append = true
         BufferedWriter writer = new BufferedWriter(exportWriter);
         for (Product product : products) {
-            System.out.println("Product: " + product.getName() + ", Deals: " + product.getDeals().size());
-            StringBuilder line = new StringBuilder();
             ArrayList<Deal> deals = product.getDeals();
             for (Deal deal : deals) {
-                line.append("\t")
-                        .append(deal.getSellerName())
-                        .append(" : ")
-                        .append(deal.getDateFrom())
-                        .append(" : ")
-                        .append(deal.getRating())
-                        .append(" : ")
-                        .append(deal.getPhoneNumber())
-                        .append("\n");
+                writer.write(
+                        deal.getSellerName() + " : " +
+                                deal.getDateFrom() + " : " +
+                                deal.getRating() + " : " +
+                                deal.getPhoneNumber() + "\n"
+                );
             }
-            writer.write(line.toString());
         }
         writer.close();
+        products.clear();
     }
-
 
 
     public static void parseProducts(List<WebElement> goods) {
         CountDownLatch latch = new CountDownLatch(goods.size());
         ExecutorService executor = Executors.newFixedThreadPool(goods.size());
-
 
         for (int i = 0; i < goods.size(); i++) {
             executor.execute(new ProductParseThread(goods.get(i).getText(), goods.get(i).getAttribute("href"), latch));
@@ -226,44 +242,40 @@ public class parser {
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\adil\\Downloads\\chromedriver-win64-124\\chromedriver-win64\\chromedriver.exe");
-
         WebDriver driver = new ChromeDriver();
         driver.get(categoryPath);
-
         int page = 1;
 
-        while (!nextFlag) {
+        while (page < 3 && !nextFlag) {
             List<WebElement> products = driver.findElements(By.className("item-card__name-link"));
-
             List<WebElement> temp = new ArrayList<>();
-
             for (int i = 0; i < products.size(); i++) {
                 temp.add(products.get(i));
-                if ((i + 1) % 3 == 0 && i != 0) {
+                if ((i + 1) % 4 == 0 && i != 0) {
                     parseProducts(temp);
                     temp.clear();
                 }
             }
-
             if (temp.size() > 0) {
                 parseProducts(temp);
             }
+
+            writeProducts();
 
             page++;
             driver.get(categoryPath + "?page=" + page);
             try {
                 boolean flag = driver.findElement(By.className("_disabled")).getText().equals("Следующая →");
-                List<WebElement> finalProducts = driver.findElements(By.className("item-card__name-link"));
-                parseProducts(finalProducts);
                 setNextFlag(flag);
             } catch(NoSuchElementException e) {
             }
-            break;
+
+            Random random = new Random();
+            int pauseTime = random.nextInt(1001) + 500;
+            Thread.sleep(pauseTime);
         }
         driver.quit();
-
-        writeProducts();
     }
 }
